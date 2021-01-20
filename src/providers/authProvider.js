42001bf670e9698397ef3e0e6c47529437db7c5f
Richard Brinkley <brinkley908@gmail.co.uk>
2021-01-20 19:39:53 +0000

    added authService, authProvider

---
+++ src/providers/authProvider.js
@@ -0,0 +1,31 @@
+/* eslint-disable prettier/prettier */
+import React, { Component } from "react";
+import AuthService from "../service/authService";
+
+const AuthContext = React.createContext({
+    signinRedirectCallback: () => ({}),
+    logout: () => ({}),
+    signoutRedirectCallback: () => ({}),
+    isAuthenticated: () => ({}),
+    signinRedirect: () => ({}),
+    signinSilentCallback: () => ({}),
+    createSigninRequest: () => ({}),
+    getToken: () => ({}),
+    getUserManager: () => ({})
+});
+
+export const AuthConsumer = AuthContext.Consumer;
+
+export class AuthProvider extends Component {
+    authService;
+    
+    constructor(props) {
+        super(props);
+        this.authService = new AuthService();
+        
+    }
+    
+    render() {
+        return <AuthContext.Provider value={this.authService}>{this.props.children}</AuthContext.Provider>;
+    }
+}
